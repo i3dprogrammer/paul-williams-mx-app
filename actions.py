b46a6857f1bd333d9f0bfd3abe2b7a60029cc567
3dprogrammer <ahmedmgd715@gmail.com>
2022-05-01 05:58:06 +0200

    Add list-users action

--- actions.py
+++ actions.py
@@ -7,7 +7,7 @@ from pprint import pprint
 def init(user, pw, host):
     return mx_platform_python.Configuration(username=user,password=pw,host=host)
 
-def create_user(configuration, email, id, metadata = '{\"first_name\": \"Steven\", \"last_name\": \"Universe\"}', disabled = False):
+def create_user(configuration, email, id, metadata, disabled = False):
     with mx_platform_python.ApiClient(configuration, 'Accept', 'application/vnd.mx.api.v1+json') as api_client:
         api_instance = mx_platform_api.MxPlatformApi(api_client)
         request_body = UserCreateRequestBody(  
@@ -25,3 +25,14 @@ def create_user(configuration, email, id, metadata = '{\"first_name\": \"Steven\
         except mx_platform_python.ApiException as e:
             print("Exception when calling MxPlatformApi->create_user: %s\n" % e)
 
+def list_users(configuration):
+    with mx_platform_python.ApiClient(configuration, 'Accept', 'application/vnd.mx.api.v1+json') as api_client:
+        api_instance = mx_platform_api.MxPlatformApi(api_client)
+        page = 1
+        records_per_page = 10
+
+        try:
+            api_response = api_instance.list_users(page=page, records_per_page=records_per_page)
+            pprint(api_response)
+        except mx_platform_python.ApiException as e:
+            print("Exception when calling MxPlatformApi->list_users: %s\n" % e)
