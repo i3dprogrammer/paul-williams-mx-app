80314d3cc2b07e31025e7d0f7e5f86e0cf3b5578
3dprogrammer <ahmedmgd715@gmail.com>
2022-05-01 05:59:00 +0200

    Change test

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ import actions, config
 
 cf = actions.init(config.CLIENT_ID, config.API_KEY, config.HOST_DEV)
 
-actions.create_user(cf, "email@provider.com", "my-unique-id", '{\"first_name\": \"Ahmed\", \"last_name\": \"Magdy\"}')
+# Create a user, we can specify any metadata.
+# actions.create_user(cf, "email@provider.com", "my-unique-id", '{\"first_name\": \"Ahmed\", \"last_name\": \"Magdy\"}')
+
+# List users
+actions.list_users(cf)
