f5a6e2e7ba45e7976945a00372d17a5da9aca25d
3dprogrammer <ahmedmgd715@gmail.com>
2022-05-01 16:28:28 +0200

    Update test

--- test.py
+++ test.py
@@ -23,4 +23,21 @@ cf = actions.init(config.CLIENT_ID, config.API_KEY, config.HOST_DEV)
 # print("------------")
 # actions.list_institute_required_creds(cf, "chase")
 
-actions.create_member(cf, "USR-f43da74d-e24f-4a36-a1cf-96fcef718c92", "chase", "my-unique-id-test", '\"credentials_last_refreshed_at\": \"2015-10-15\"')
+# actions.create_member(cf, "USR-f43da74d-e24f-4a36-a1cf-96fcef718c92", "chase", "my-unique-id-test", '\"credentials_last_refreshed_at\": \"2015-10-15\"')
+# {'member': {'aggregated_at': None,
+#             'connection_status': 'CREATED',
+#             'guid': 'MBR-65c52665-33e5-4971-8d80-5c28010db001',
+#             'id': 'my-unique-id-test',
+#             'institution_code': 'chase',
+#             'is_being_aggregated': True,
+#             'is_managed_by_user': True,
+#             'is_oauth': False,
+#             'metadata': '"credentials_last_refreshed_at": "2015-10-15"',
+#             'name': 'Chase Bank',
+#             'successfully_aggregated_at': None,
+#             'user_guid': 'USR-f43da74d-e24f-4a36-a1cf-96fcef718c92',
+#             'user_id': 'williamp0044'}}
+
+actions.list_accounts(cf, 'USR-f43da74d-e24f-4a36-a1cf-96fcef718c92')
+print('-----------')
+actions.list_members(cf, 'USR-f43da74d-e24f-4a36-a1cf-96fcef718c92')
